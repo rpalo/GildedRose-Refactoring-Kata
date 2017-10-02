2566f219deb81239262552a5630c186a3ae98af0
Ryan Palo <ryan@thepalos.com>
2017-10-01 23:10:42 -0700

    Refactor to special cases hash

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,34 +1,46 @@
 # The Gilded Rose Store Inventory
 class GildedRose
+  SPECIAL_CASES = {
+    'Aged Brie' => :process_brie,
+    'Sulfuras, Hand of Ragnaros' => :process_sulfuras,
+    'Backstage passes to a TAFKAL80ETC concert' => :process_tickets
+  }
+
   def initialize(items)
     @items = items
   end
 
   def update_quality
     @items.each do |item|
-      if item.name == 'Aged Brie'
-        item.quality = [item.quality + 1, 50].min
-        item.sell_in -= 1
-      elsif item.name == 'Sulfuras, Hand of Ragnaros'
-        item.quality = 80
-      elsif item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        case item.sell_in
-        when (-Float::INFINITY..0)
-          item.quality = 0
-        when (1..5)
-          item.quality += 3
-        when (6..10)
-          item.quality += 2
-        else
-          item.quality += 1
-        end
-        item.sell_in -= 1
+      special_action = SPECIAL_CASES.fetch(item.name, nil)
+      if special_action
+        send(special_action, item)
       else
         reduction = item.sell_in <= 0 ? 2 : 1
         item.quality = [item.quality - reduction, 0].max
         item.sell_in -= 1
       end
     end
+  end
+
+  def process_brie(item)
+    item.quality = [item.quality + 1, 50].min
+    item.sell_in -= 1
+  end
+
+  def process_sulfuras(item)
+    item.quality = 80
+  end
+
+  def process_tickets(item)
+    item.quality = 
+      case item.sell_in
+      when (-Float::INFINITY..0) then 0
+      when (1..5) then item.quality + 3
+      when (6..10) then item.quality + 2
+      when (11..Float::INFINITY) then item.quality + 1
+      end
+    item.sell_in -= 1
   end
 end
 
